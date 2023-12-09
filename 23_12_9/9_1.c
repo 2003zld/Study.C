781ce4583bc68b822fe42eef740388fabb73fee1
2003zld <119653554+2003zld@users.noreply.github.com>
2023-12-09 19:08:48 +0800

    Add files via upload

---
+++ 23_12_9/9_1.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int n = 0;
+	int i = 0;
+	int sum = 0;
+	printf("请输入一个正整数：");
+	scanf("%d", &n);
+	for (i = 0; i <= n; i++)
+	{
+		sum += i;
+	}
+	printf("结果为%d\n", sum);
+	return 0;
+}
---
+++ 23_12_9/9_2.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int i = 0;
+	int n = 0;
+	int sum = 0;
+	scanf("%d", &n);
+	while (i <= n)
+	{
+		sum += i;
+		i++;
+	}
+	printf("%d", sum);
+	return 0;
+}
